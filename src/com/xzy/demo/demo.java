229abfdb246cd5e6024908fd5d66d7902c74c76e
w2719628312 <15030556440@163.com>
2019-07-31 17:41:01 +0800

    123

---
+++ src/com/xzy/demo/demo.java
@@ -0,0 +1,9 @@
+package com.xzy.demo;
+
+public class demo<sout> {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println();
+    }
+
+}
